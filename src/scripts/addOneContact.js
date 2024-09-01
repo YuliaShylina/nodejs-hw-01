95869b487e1c0bde526ea5d39dec9f97476d5c48
YuliaShylina <yuliyashylina@gmail.com>
2024-09-01 13:00:00 +0300

    change AddOne contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -10,7 +10,7 @@
 
 import { createFakeContact } from '../utils/createFakeContact.js';
 import { getAllContacts } from './getAllContacts.js';
-import { updateContacts } from './updateContacts.js';
+import { updateContacts } from '../utils/updateContacts.js';
 
 export const addOneContact = async () => {
   const contaсtsList = await getAllContacts();
